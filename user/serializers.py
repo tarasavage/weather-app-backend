23d5b47337bb39df069389a62f394a0d8fac5da5
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-04-27 16:33:55 +0300

    Added update & partial update for user by api/user/me

--- user/serializers.py
+++ user/serializers.py
@@ -18,4 +18,16 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
+        """Create user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update user with correctly encrypted password"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
